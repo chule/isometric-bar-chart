be013feaad8794a5ede73fd9fe492095b29ee73b
Igor Cuckovic <cuckovic@gmail.com>
2023-07-10 03:20:04 +0200

    package update

---
+++ dist/cjs/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+var Barchart_1 = tslib_1.__importDefault(require("./Barchart"));
+exports.default = Barchart_1.default;
+//# sourceMappingURL=index.js.map
